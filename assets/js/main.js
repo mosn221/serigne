9251c3ae58c39188c1ab1c7065eb28b99c49f09b
mosn221 <168453277+mosn221@users.noreply.github.com>
2025-12-26 05:15:17 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -276,7 +276,3 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const el = document.getElementById("copyright-year");
-  if (el) el.textContent = new Date().getFullYear();
-});
