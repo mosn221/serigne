e90162c04e323ba4cc95e7bd491a3c9c0e61a392
mosn221 <168453277+mosn221@users.noreply.github.com>
2026-02-08 03:15:32 +0100

    Update main-clean.js

--- assets/js/main-clean.js
+++ assets/js/main-clean.js
@@ -119,10 +119,3 @@
   window.addEventListener("load", navActiveOnScroll);
   window.addEventListener("scroll", navActiveOnScroll);
 
-  /**
-   * Dynamic copyright year
-   
-  const yearEl = document.getElementById("copyright-year");
-  if (yearEl) yearEl.textContent = new Date().getFullYear();
-})();
-*/
